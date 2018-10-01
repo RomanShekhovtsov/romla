709093a33538c5d5b070b770646023a053e3dcce
SBT-Shekhovtsov-RV <pomka@yandex.ru>
2018-10-01 19:31:25 +0300

    del low correlated features

---
+++ TROT_install.cmd
@@ -0,0 +1,3 @@
+pip install deap update_checker tqdm stopit --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org
+conda install pywin32
+pip install tpot --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org
